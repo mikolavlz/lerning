fa512cd07977e8b12f2e6f22509651308a075b6b
Николай Локтионов <nikolai.loktionov@gmail.com>
2022-12-27 20:18:25 +0300

    добавил фукции

--- lesson5/devo function2.py
+++ lesson5/devo function2.py
@@ -1,6 +1,17 @@
-def hello(fio):
-    global name
-    name = fio
-hello("Алексей")
-hello ("Роман")
-print("hello,", name)
+# def hello(fio):
+#     global name
+#     name = fio
+# hello("Алексей")
+# hello ("Роман")
+# print("hello,", name)
+
+# def sum_number(a,b,*x):
+#     s = a + b
+#     for i in x:
+#         s += i
+#     print(s)
+#     pass
+sum_number(1,2,3,4,5)
+ def func():
+     return 10,20
+print(func()[0])
