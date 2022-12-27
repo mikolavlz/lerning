86f52354143cd0e1d389bbc5f556e3370c76c4b8
Николай Локтионов <nikolai.loktionov@gmail.com>
2022-12-27 18:44:18 +0300

    начал с 3 числа

--- home_work/3-4/pifagot table.py
+++ home_work/3-4/pifagot table.py
@@ -1,4 +1,4 @@
-for i in range(1,11):
-    for n in range (1,11):
+for i in range(1,10):
+    for n in range (1,10):
         print(f"| {i} * {n} = {i * n} ",end='')
     print()
---
+++ lesson5/demo for else.py
@@ -0,0 +1,11 @@
+is_find = False
+for i in "hello word":
+    if i == "t":
+        is_find == True
+        print("Буква есть")
+        break
+else:
+        print("Буквы нет")
+
+
+
