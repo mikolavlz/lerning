e3a828db35c0bb4bcb329115e671e1e5e9f6fb44
Николай Локтионов <nikolai.loktionov@gmail.com>
2023-01-19 21:01:23 +0300

    урок9+

--- lesson9/files/index.py
+++ lesson9/files/index.py
@@ -1,6 +1,19 @@
-# сначала фаил открыть
-file = open('c:/test/test.txt','w')
-print(type(file))
-#для записи используем write
-file.write('Java\nPython\nPHP')
-file.close()
+# # сначала фаил открыть
+# file = open('c:/test/test.txt','w')
+# print(type(file))
+# #для записи используем write
+# file.write('Java\nPython\nPHP')
+# file.close()
+
+# # второй способ
+# with open('c:/test/test.txt','w') as file :
+#     file.writelines(['newstr1\n','newstr2\n','newstr3\n'])
+
+# считываем строки
+# read- считывает
+# readline- считывает первую строку
+# readlines - сяитываем строки в список и досуп по индексу
+with open('c:/test/test.txt','r') as file :
+    # print(file.readlines()[2])
+    for line in file:
+        print(line,end=' ')
