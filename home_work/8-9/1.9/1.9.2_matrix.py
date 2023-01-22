2d4117b878e35fe61ab0584f3e06dc29c192e8ed
Николай Локтионов <nikolai.loktionov@gmail.com>
2023-01-22 19:35:52 +0300

    HW9+

--- home_work/8-9/1.9/1.9.2_matrix.py
+++ home_work/8-9/1.9/1.9.2_matrix.py
@@ -1,8 +1,13 @@
-with open('matr.txt','r') as file :
+i = 1
+with open('matr.txt', 'r') as file:
+    list_2 = []
     for line in file:
-        print(line)
-        list = line.split(' ')
-        for i in range(0,5):
+        list_i = line.split()
+        for item in list_i:
+            list_2.append((int(item)**i))
+        i += 1
+with open('matr.txt', 'a') as file:
+    file.write('\n')
+    for n in range(0, 7, 3):
+        file.write(f'{list_2[n]}  {list_2[n+1]}  {list_2[n+2]}\n')
 
-            # a = int(line[i])**2
-            print(line[i])
