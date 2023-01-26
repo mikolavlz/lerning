76499df4625a8ee8b0c2bc717918c829925f890f
Николай Локтионов <nikolai.loktionov@gmail.com>
2023-01-26 20:30:16 +0300

    lesson11 -  exeption 2.0

--- lesson11/DB/exceptions.py
+++ lesson11/DB/exceptions.py
@@ -12,16 +12,81 @@ num2 = randint(0,2)
 #     print('делениена ноль')
 # print('строка')
 
-while True:
-    first_num = input("первое число")
-    if first_num == 'q':
-        break
-    second_num = input("второе число")
-    if first_num == 'q':
-        break
-    try:
-        res = int(first_num)/int(second_num)
-    except ZeroDivisionError:
-        print('делениена ноль')
+# while True:
+#     first_num = input("первое число")
+#     if first_num == 'q':
+#         break
+#     second_num = input("второе число")
+#     if first_num == 'q':
+#         break
+#     try:
+#         res = int(first_num)/int(second_num)
+#     except ZeroDivisionError:
+#         print('делениена ноль')
+#     else:
+#         print("частное  = ", res)
+
+# try:
+#     x = int(input("thislo"))
+#     res = 10 / x
+#
+#     print(res)
+# except ZeroDivisionError:
+#     print("на ноль")
+# except ValueError:
+#     print("некоректное значение")
+# except:
+#     print("error")
+
+# пример 3. расшифровка ошибок
+#
+# try:
+#     x = int(input("thislo"))
+#     res = 10 / x
+#     print(res)
+# except ZeroDivisionError as test:
+#     print("на ноль {}".format(test))
+# except ValueError as test2:
+#     print("некоректное значение {}".format(test2))
+# except:
+#     print("error")
+
+# #4 пример
+# my_file = 'test.txt'
+# try:
+#     with open(my_file,encoding="utf-8") as file:
+#         content = file.readlines()
+# except FileNotFoundError:
+#     print("Фаил не найден")
+#
+#
+
+# 5пример
+# try:
+#     num = int(input("введите число"))
+#     assert num % 2 == 0
+# except:
+#     print("Число не четное")
+# else:
+#     print(1 / num)
+
+# после try можно вместо except испольовать finally -инструкции выполняються обчзательно
+# try:
+#     file = open('test.txt')
+#     content = file.readlines()
+# finally:
+#     file.close()
+
+# генерация исключений
+
+# если мы хотим напугать пользователя ошибкой используем raise
+try:
+    age = int(input("введи возраст от 20 -25"))
+    if 20 <= age <= 25:
+        print('ok')
     else:
-        print("частное  = ", res)
+        raise ValueError('Вы ввели неправильный возраст')
+except Exception as e:
+    # print(f"Возникла ошибка{e}")
+    print(e)
+print(111)
