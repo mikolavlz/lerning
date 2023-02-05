c592e58bbfe27265ac56ca4536bfa6b6f76a1215
Николай Локтионов <nikolai.loktionov@gmail.com>
2023-02-05 17:58:36 +0300

    1.13.1

--- home_work/13/office.py
+++ home_work/13/office.py
@@ -1,3 +1,4 @@
+import string
 class Office:
     # В конструкторе описываются поля класса(свойства объекта)
     # Поля класса доступны в любом нестатическом методе, то есть в любом, где
@@ -15,8 +16,12 @@ class Office:
             i += 1
         man_max = self.get_man_max_salary()
         man_min = self.get_man_min_salary()
-        print(f"Сотрудник {man_max.fio} имеет максимальный оклад {man_max.salary}")
-        print(f"Сотрудник {man_min.fio} имеет максимальный оклад {man_min.salary}")
+        man60k =  self.get_salary_above_60k()
+        # print(f"Сотрудник {man_max.fio} имеет максимальный оклад {man_max.salary}")
+        # print(f"Сотрудник {man_min.fio} имеет минимальный оклад {man_min.salary}")
+        for l in range(len(man60k)):
+            print(f"Сотрудники {man60k[l].fio} имеет оклад более 60000 = {man60k[l].salary}")
+
     # Получаем сотрудника с макс. окладом
     def get_man_max_salary(self):
         # Пусть первый сотрудник имеет макс. оклад
@@ -33,6 +38,17 @@ class Office:
             if self.persons[i].salary < man_min.salary:
                 man_min = self.persons[i]
         return man_min #методу присвоили сотрудника с мин. окладом
+    def get_salary_above_60k(self):
+        man60k = []
+        print("\n")
+        for i in range(1,
+            len(self.persons)):  # обошли всех сотрудников, начиная со 1го и сравнили каждого с 60000
+            if self.persons[i].salary > 60000:
+                man60k.append(self.persons[i])
+        return man60k
+
+
+
 
 class Person:
     # self - это ссылка на объект, который мы заполняем свойствами после его создания
@@ -53,9 +69,16 @@ class Person:
 man1 = Person("Иванов",30,70000)
 man2 = Person("Петров",36,90000)
 man3 = Person("Сидоров",32,80000)
+man4 = Person("Егоров",32,30000)
+man5 = Person("Милонов",32,99000)
+man6 = Person("Курянов",32,60000)
+man7 = Person("Жлобов",32,20000)
+man8 = Person("Котов",32,60001)
 
-men = [man1,man2,man3]
+
+men = [man1,man2,man3,man4,man5,man6,man7,man8]
 
 
 company = Office("IT Start",men)
 company.show_info() #выведем всю информацию об офисе
+
