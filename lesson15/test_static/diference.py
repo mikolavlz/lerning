01a3b9d0e092caf503de2063071acd99ba6534eb
Николай Локтионов <nikolai.loktionov@gmail.com>
2023-02-09 21:01:00 +0300

    diference

--- lesson15/test_static/diference.py
+++ lesson15/test_static/diference.py
@@ -1,17 +1,18 @@
-class Test:
-    x = 0
+# class Test:
+#     x = 0
+#
+#
+#     def f(self):
+#         Test.x += 1
+#         print(Test.x)
+#
+#
+# obj1 = Test()
+# obj2 = Test()
+#
+# obj1.f()
+# obj1.f()
 
+# obj2.f()
+# obj2.f()
 
-    def f(self):
-        Test.x += 1
-        print(Test.x)
-
-
-obj1 = Test()
-obj2 = Test()
-
-obj1.f()
-obj1.f()
-
-obj2.f()
-obj2.f()
