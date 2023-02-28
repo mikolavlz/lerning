21ff0143d164f81edd30c5cfe7c0f3b5fada8e35
Николай Локтионов <nikolai.loktionov@gmail.com>
2023-02-28 21:01:26 +0300

    lesson 19.5

--- lesson 19/algoritms/bubles.py
+++ lesson 19/algoritms/bubles.py
@@ -1,8 +1,6 @@
 def my_sort(arr):
     for i in range(1, len(arr)):
-        for j in range (0, len(arr) -1):
-            if arr[j] > arr[j +1]:
-                arr[j],arr[j +1] = arr[j+1] , arr[j]
+
     return arr
 
 print(my_sort([3,5,7,87,9,5,76,45,3]))
