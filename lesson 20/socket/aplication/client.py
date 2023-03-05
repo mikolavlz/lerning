ae5de2daad440c6d1e7d88500512a4c04ad72404
Николай Локтионов <nikolai.loktionov@gmail.com>
2023-03-05 18:13:09 +0300

    lesson 20.hw

--- lesson 20/socket/aplication/client.py
+++ lesson 20/socket/aplication/client.py
@@ -2,7 +2,18 @@ from socket import *
 
 s = socket(AF_INET,SOCK_STREAM) #создали сокет
 s.connect(('localhost',8007))
-msg = '2 6'
+while True:
+    msg = input("Введи 1, 2, 3 или q")
+    if msg == "1" or msg == "2" or msg == "3" or msg == 'q':
+        if msg == 'q':
+            break
+        else:
+            s.send(msg.encode('UTF-8'))
+            answer = s.recv(1000000000)
+            print(answer.decode('UTF-8'))
+    else:
+        print('Внимательно следите за тем что вводите')
+
 s.send(msg.encode('UTF-8'))
 answer = s.recv(1000000000)
 print('Сообщение от сервера: ',answer.decode('UTF-8'))
