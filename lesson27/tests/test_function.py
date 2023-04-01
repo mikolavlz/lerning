813eb881dae39ec0677804546f23dd5f95fb376c
Николай Локтионов <nikolai.loktionov@gmail.com>
2023-04-02 00:22:33 +0300

    api methods _lesson 27

---
+++ lesson27/tests/test_function.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_sum(self):
+        self.assertEqual(sum(3, 5), 7)
+
+
+class Test(TestCase):
+    def test_comp(self):
+        self.assertEqual(com)
